0bb353b14411c9fa330602cd0a10129e83d18ab9
Kostiantyn <kostiantyn.naftulovych@iisviolamarchesini.edu.it>
2024-04-11 11:23:23 +0200

    Thread ex 2

--- Thread/saluti1.c
+++ Thread/saluti1.c
@@ -6,27 +6,29 @@
 //Thread
 //POSIX - una standart di UNIX
 void* StampaBuongiorno(void* par){
-    printf("Buongiorno\n");
+    printf("%s\n", (char*)par);
     return NULL;
 }
 void* StampaBuonasera(void* par){
-    printf("Buonasera\n");
+    printf("%s\n", (char*)par);
     return NULL;
 }
 void* StampaBuonanotte(void* par){
-    printf("Buonanotte\n");
+    printf("%s\n", (char*)par);
     return NULL;
 }
 
 int main(){
 
     pthread_t threadGiorno, threadSera, threadNotte;
-
+    char* msg1 = "boungiorno";
+    char* msg2 = "bounasera";
+    char* msg3 = "bounanotte";
     printf("Ciao\n");
 
-    pthread_create(&threadGiorno, NULL, &StampaBuongiorno, NULL);
-    pthread_create(&threadSera, NULL, &StampaBuonasera, NULL);
-    pthread_create(&threadNotte, NULL, &StampaBuonanotte, NULL);
+    pthread_create(&threadGiorno, NULL, &StampaBuongiorno, msg1);
+    pthread_create(&threadSera, NULL, &StampaBuonasera, msg2);
+    pthread_create(&threadNotte, NULL, &StampaBuonanotte, msg3);
 
     pthread_join(threadGiorno, NULL);
     pthread_join(threadSera, NULL);
